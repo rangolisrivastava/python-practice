db1e32aa515a1f225ce99f68d3990527a03b7b7c
rangolisrivastava <srivastava.rangoli.rs@gmail.com>
2026-08-24 04:49:53 +0530

    add this pyhton file

---
+++ mod-2/range.py
@@ -0,0 +1,24 @@
+for i in range(1, 21, 2):
+    print(i)
+for i in range (1, 5):
+    if i % 2 == 0:
+        print(i)
+
+i = 1
+while i <= 7:
+    print(i)
+    i += 1 #if this line absent infinite loop formed
+
+print("end of the code")
+
+i = 1
+while i <= 7:
+    print(i * ";)") #triangle
+    i += 1
+print("end")
+
+i = 7
+while i > 0:
+    print(i * ":-)") #reverse triangle
+    i -= 1
+print("end")
