c9a2a1690593175645fb4dc3a8acb402373ba714
rangolisrivastava <srivastava.rangoli.rs@gmail.com>
2026-08-24 01:23:36 +0530

    add python file

---
+++ firstCode/code.py
@@ -0,0 +1,14 @@
+name = "wanda maximof"
+
+print (name.find("d"))
+print(name.replace("max", "wald"))
+
+print("x" in name)
+a = 99.5
+b = 23.75
+c = 16.15
+
+total = a + b + c
+print("total:", total)
+avg = total/3
+print("avg price:", avg)
